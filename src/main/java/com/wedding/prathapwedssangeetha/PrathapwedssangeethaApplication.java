901795a00532b7327138ba8712c4f8d3d4ebfd5a
Prathap.Shivakumar <Prathap.Shivakumar@target.com>
2020-11-30 00:02:22 +0530

    initial comit

---
+++ src/main/java/com/wedding/prathapwedssangeetha/PrathapwedssangeethaApplication.java
@@ -0,0 +1,13 @@
+package com.wedding.prathapwedssangeetha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrathapwedssangeethaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrathapwedssangeethaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedding/prathapwedssangeetha/PrathapwedssangeethaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.prathapwedssangeetha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrathapwedssangeethaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
